1197a0bf0ee609d92d651a359c6d9296f198e2cb
sheshbzzarr <demerwdani@gmail.com>
2024-12-08 14:39:02 -0500

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,14 +4,15 @@ A module that contains a function that returns the perimeter of an island
 described in grid
 """
 
-
 def island_perimeter(grid):
     """A function that outputs the perimeter of an island described by grid"""
     perimeter = 0
     grid_length = len(grid)
+    
     for row in range(grid_length):
         for column in range(len(grid[row])):
             if grid[row][column] == 1:
+                # Check top, left, right, bottom cells
                 if row - 1 < 0 or grid[row - 1][column] == 0:
                     perimeter += 1
                 if column - 1 < 0 or grid[row][column - 1] == 0:
